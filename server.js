7e2fc2f688d79588bd6a76c1f61e0578c553f20f
Sani <idriss2004@hotmail.com>
2017-12-23 18:20:27 +0100

    use express server

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
-var http = require("http");
-http.createServer(function(request, response){
-    response.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
-    response.write('hello world');
-    response.end()
-    console.log("Node ")
-}).listen(1337);
+var express = require("express");
+var app = express();
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname+'/index.html')
+});
+
+app.get("/status", function(req,res){
+    res.send("OK")
+});
+const port = 1337;
+
+app.listen(port, function(){
+    console.log(`Express node js server running on ${port}`)
+})
+// http.createServer(function(request, response){
+//     response.writeHead(200, {
+//         'Content-Type': 'text/plain'
+//     });
+//     response.write(request.url);
+//     response.end()
+//     console.log("Node server running on port 1337")
+// }).listen(1337);
